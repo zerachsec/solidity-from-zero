a804605bf4bb238b0595bf43b33a169af6076ab6
zerachsec <zerach.sec@gmail.com>
2026-02-04 16:23:58 +0530

    tuples challenge completed

--- Tupledore/src/Tupledore.sol
+++ Tupledore/src/Tupledore.sol
@@ -3,13 +3,29 @@ pragma solidity ^0.8.13;
 
 contract Tupledore {
     /* This exercise assumes you know about tuples/struct in solidity.
-        1. Create a struct named `UserInfo` with types address 
+        1. Create a struct named `UserInfo` with types address
            and uint256.
         2. Create a variable of type UserInfo, named `userInfo`.
-        3. Create a function called `setTuple` that takes in 
-           a address and uint256 and sets the all values 
+        3. Create a function called `setTuple` that takes in
+           a address and uint256 and sets the all values
            the `userInfo` variable you created above.
-        4. Create a function called `returnTuple`, 
+        4. Create a function called `returnTuple`,
            that returns `userInfo` (as a tuple)
     */
+
+    struct UserInfo {
+        address user;
+        uint256 score;
+    }
+
+    UserInfo public userInfo;
+
+    function setTuple(address _user, uint256 _score) public {
+        userInfo.user = _user;
+        userInfo.score = _score;
+    }
+
+    function returnTuple() public view returns (address, uint256) {
+        return (userInfo.user, userInfo.score);
+    }
 }
