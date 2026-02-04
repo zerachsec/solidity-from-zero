09cc9f59da8b0789826e05af54c95237729c6c11
zerachsec <zerach.sec@gmail.com>
2026-02-04 22:18:28 +0530

    withdraw challenge completed

--- Withdraw/src/Withdraw.sol
+++ Withdraw/src/Withdraw.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.8.13;
 
 contract Withdraw {
+    receive() external payable {}
+
     // @notice make this contract able to receive ether from anyone and anyone can call withdraw below to withdraw all ether from it
     function withdraw() public {
         // your code here
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
