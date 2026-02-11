13773756dc9a988cfbe5c6e5ec4c3e5c20a737b7
zerachsec <zerach.sec@gmail.com>
2026-02-11 21:47:11 +0530

    distribute challenge compl[ete

--- DistributeV2/src/DistributeV2.sol
+++ DistributeV2/src/DistributeV2.sol
@@ -2,18 +2,25 @@
 pragma solidity ^0.8.13;
 
 contract DistributeV2 {
-    /*
-        This exercise assumes you know how to sending Ether.
-        1. This contract has some ether in it, distribute it equally among the
-           array of addresses that is passed as argument.
-        2. Write your code in the `distributeEther` function.
-        3. Consider scenarios where one of the recipients rejects the ether transfer, 
-           have a work around for that whereby other recipients still get their transfer
-    */
-
     constructor() payable {}
 
     function distributeEther(address[] memory addresses) public {
-        // your code here
+        uint256 count = addresses.length;
+        require(count > 0, "No recipients");
+
+        uint256 balance = address(this).balance;
+        uint256 share = balance / count;
+
+        require(share > 0, "Insufficient balance");
+
+        for (uint256 i = 0; i < count; i++) {
+            (bool success,) = payable(addresses[i]).call{value: share}("");
+
+            // If transfer fails, skip and continue
+            if (!success) {
+                continue;
+            }
+        }
+        // Any remainder stays in contract
     }
 }
