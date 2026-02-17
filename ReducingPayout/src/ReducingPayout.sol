dce44ef62b57a7fe42e95910fe9e7f4be35027f4
zerachsec <zerach.sec@gmail.com>
2026-02-17 21:29:14 +0530

    challenge complted

--- ReducingPayout/src/ReducingPayout.sol
+++ ReducingPayout/src/ReducingPayout.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 contract ReducingPayout {
     /*
         This exercise assumes you know how block.timestamp works.
-        1. This contract has 1 ether in it, each second that goes by, 
+        1. This contract has 1 ether in it, each second that goes by,
            the amount that can be withdrawn by the caller goes from 100% to 0% as 24 hours passes.
         2. Implement your logic in `withdraw` function.
         Hint: 1 second deducts 0.0011574% from the current %.
@@ -19,5 +19,10 @@ contract ReducingPayout {
 
     function withdraw() public {
         // your code here
+        uint256 timeElapsed = block.timestamp - depositedTime;
+        require(timeElapsed < 24 hours, "Payout period is over");
+        uint256 payoutPercentage = 100 - (timeElapsed * 11574) / 1000000; // 0.0011574% per second
+        uint256 payoutAmount = (address(this).balance * payoutPercentage) / 100;
+        payable(msg.sender).transfer(payoutAmount);
     }
 }
