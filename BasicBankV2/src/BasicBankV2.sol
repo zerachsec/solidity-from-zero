a0602b14e46c299d59236bf68db098a707fc5ced
zerachsec <zerach.sec@gmail.com>
2026-02-18 17:59:10 +0530

    challenge completed

--- BasicBankV2/src/BasicBankV2.sol
+++ BasicBankV2/src/BasicBankV2.sol
@@ -8,9 +8,21 @@ contract BasicBankV2 {
 
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    function addEther() external payable {
+        // Update the user's balance with the deposited amount
+        balances[msg.sender] += msg.value;
+    }
 
     /// @notice used to withdraw ether from the contract
     /// @param amount of ether to remove. Cannot execeed balance i.e users cannot withdraw more than they deposited
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+        // Check if the user has enough balance to withdraw
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+
+        // Update the user's balance before transferring to prevent reentrancy attacks
+        balances[msg.sender] -= amount;
+
+        // Transfer the specified amount of ether to the user
+        payable(msg.sender).transfer(amount);
+    }
 }
