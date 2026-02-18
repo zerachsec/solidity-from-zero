be788aa5c465d8aaad40dbfc4ed800d2c531cb00
zerachsec <zerach.sec@gmail.com>
2026-02-18 17:58:17 +0530

    challenge completed

--- BasicBank/src/BasicBank.sol
+++ BasicBank/src/BasicBank.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.13;
 contract BasicBank {
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    function addEther() external payable {
+        // No need to do anything special here since msg.value is automatically added to the contract's balance
+    }
 
     /// @notice used to withdraw ether from the contract (No restriction on withdrawals)
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+        payable(msg.sender).transfer(amount);
+    }
 }
