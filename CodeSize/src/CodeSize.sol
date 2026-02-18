878d7d7c645826b6405a9c8621250ff7ba181799
zerachsec <zerach.sec@gmail.com>
2026-02-18 18:06:47 +0530

    challenge completed

--- CodeSize/src/CodeSize.sol
+++ CodeSize/src/CodeSize.sol
@@ -5,4 +5,22 @@ contract CodeSize {
     /**
      * The challenge is to create a contract whose runtime code (bytecode) size is greater than 1kb but less than 4kb
      */
+    bytes32[64] private data;
+
+    constructor() {
+        for (uint256 i = 0; i < data.length; i++) {
+            data[i] = keccak256(abi.encodePacked(i));
+        }
+    }
+
+    function getData(uint256 index) external view returns (bytes32) {
+        require(index < data.length, "Out of bounds");
+        return data[index];
+    }
+
+    function sumHashes() external view returns (bytes32 result) {
+        for (uint256 i = 0; i < data.length; i++) {
+            result = keccak256(abi.encodePacked(result, data[i]));
+        }
+    }
 }
