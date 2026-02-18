de90aec70afd4de5c7f6c86da09426e599a3d319
zerachsec <zerach.sec@gmail.com>
2026-02-18 18:12:14 +0530

    challenge completed

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract TicTacToe {
-    /* 
+    /*
         This exercise assumes you know how to manipulate nested array.
         1. This contract checks if TicTacToe board is winning or not.
         2. Write your code in `isWinning` function that returns true if a board is winning
@@ -12,5 +12,29 @@ contract TicTacToe {
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
         // your code here
+        // Check rows
+        for (uint8 i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != 0) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (uint8 j = 0; j < 3; j++) {
+            if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != 0) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0) {
+            return true;
+        }
+
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != 0) {
+            return true;
+        }
+
+        return false;
     }
 }
